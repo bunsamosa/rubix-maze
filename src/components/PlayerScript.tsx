6a7c75dd3067f875a563ad144ff777a5bbe4673e
tejaaswini <tejnaren07@gmail.com>
2022-05-21 19:24:10 +0530

    update(keys): remove non arrow control

--- src/components/PlayerScript.tsx
+++ src/components/PlayerScript.tsx
@@ -20,10 +20,10 @@ export default function PlayerScript() {
     const [pathOverlayEnabled, setPathOverlayEnabled] = useState(true);
 
     // key controls
-    const leftKey = useKeyPress(['ArrowLeft', 'a']);
-    const rightKey = useKeyPress(['ArrowRight', 'd']);
-    const upKey = useKeyPress(['ArrowUp', 'w']);
-    const downKey = useKeyPress(['ArrowDown', 's']);
+    const leftKey = useKeyPress(['ArrowLeft']);
+    const rightKey = useKeyPress(['ArrowRight']);
+    const upKey = useKeyPress(['ArrowUp']);
+    const downKey = useKeyPress(['ArrowDown']);
 
     useGameLoop(() => {
         const direction = {
